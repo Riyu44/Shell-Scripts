1896523a24b3563ba166d4c91dd41f44863181cc
Priyansh Shrivastava <priyansh.shrivastava@ext.soprabanking.com>
2023-06-07 15:13:30 +0530

    fixes the file token access

--- basic/pract.sh
+++ basic/pract.sh
@@ -4,7 +4,7 @@
 
 # access="$(curl -X POST "https://accounts.spotify.com/api/token" \
 #      -H "Content-Type: application/x-www-form-urlencoded" \
-#      -d "grant_type=client_credentials&client_id=a07a807ea6bc47499ab66177cd36e371&client_secret=b232e2a7da4c4bc79e6cb38a15877b1c" \
+#      -d "grant_type=client_credentials&client_id=CLIENT_ID" \
 #      | jq '. | .access_token')"  
 # echo $access
 # curl "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" \
